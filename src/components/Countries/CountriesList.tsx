c939ba8630c30471dc9702871eaf57084e9629a2
HamidReza <delshad.swdn@gmail.com>
2023-01-05 18:14:37 +0100

    add sort function

--- src/components/Countries/CountriesList.tsx
+++ src/components/Countries/CountriesList.tsx
@@ -4,12 +4,16 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RootState, AppDispatch } from "../../redux/store";
 import fetchCountryData from "../../redux/thunk/country";
 import CountryItem from "./CountryItem";
+import { actions } from "./../../redux/slice/country";
+import { IconButton, Tooltip } from "@mui/material";
 
 type PropType = {
   userInput: string;
@@ -44,10 +48,12 @@ function createData(
 }
 
 export default function CountriesList({ userInput }: PropType) {
+  const [sortBtn, setSortBtn] = useState(false);
   const countriesList = useSelector(
     (state: RootState) => state.country.countries
   );
   const dispatch = useDispatch<AppDispatch>();
+  const dipatchNorm = useDispatch();
   useEffect(() => {
     if (userInput === "") {
       dispatch(fetchCountryData());
@@ -77,32 +83,89 @@ export default function CountriesList({ userInput }: PropType) {
     );
   });
 
+  const ascSortedCoutry = () => {
+    setSortBtn(true);
+    countryRows.sort((a, b) => {
+      const nameA = a.name.common.toLowerCase();
+      const nameB = b.name.common.toLowerCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    dipatchNorm(actions.getCountryData(countryRows));
+  };
+
+  const decSortedCoutry = () => {
+    setSortBtn(false);
+    countryRows.sort((a, b) => {
+      const nameA = a.name.common.toLowerCase();
+      const nameB = b.name.common.toLowerCase();
+      if (nameA < nameB) {
+        return 1;
+      }
+      if (nameA > nameB) {
+        return -1;
+      }
+      return 0;
+    });
+    dipatchNorm(actions.getCountryData(countryRows));
+  };
+
   return (
-      <div>
-        {countriesList.length === 0 && (
-          <div>
-            <i className="fas fa-spinner fa-spin fa-xl" />
-            <p style={{ marginTop: "10px" }}>Loading...</p>
-          </div>
-        )}
-        <TableContainer component={Paper} style={{ marginTop: "50px" }}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                {tableHeader.map((title) => (
-                  <TableCell align="center" key={crypto.randomUUID()}>
-                    <strong>{title}</strong>
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            {countryRows.map((country) => {
-              return (
-                <CountryItem key={crypto.randomUUID()} country={country} />
-              );
-            })}
-          </Table>
-        </TableContainer>
-      </div>
+    <div>
+      {countriesList.length === 0 && (
+        <div>
+          <i className="fas fa-spinner fa-spin fa-xl" />
+          <p style={{ marginTop: "10px" }}>Loading...</p>
+        </div>
+      )}
+      <TableContainer component={Paper} style={{ marginTop: "50px" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">
+                <strong>Flag</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Name</strong>
+                <Tooltip title="Sort by name">
+                  {sortBtn ? (
+                    <IconButton onClick={decSortedCoutry}>
+                      <ArrowDownwardIcon fontSize="small" />
+                    </IconButton>
+                  ) : (
+                    <IconButton onClick={ascSortedCoutry}>
+                      <ArrowUpwardIcon fontSize="small" />
+                    </IconButton>
+                  )}
+                </Tooltip>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Region</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Population</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Languages</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Favorite</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>More</strong>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          {countryRows.map((country) => {
+            return <CountryItem key={crypto.randomUUID()} country={country} />;
+          })}
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
